5c963b0bce551429a43ce97c954752ebfb959d4b
abhinaba900 <abhinabajana900@gmail.com>
2024-02-10 23:33:53 +0530

    frontend link added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const useragent = require("express-useragent");
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://final-frontand.vercel.app"],
     credentials: true,
   })
 );
